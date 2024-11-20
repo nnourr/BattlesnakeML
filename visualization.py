a8b9f5a10196b33f8d9b186536a9c8ff740742a4
lee-ranaweer <lee.ranaweer@gmail.com>
2024-11-20 14:10:59 -0500

    Single responsibility

--- visualization.py
+++ visualization.py
@@ -1,14 +1,24 @@
-from Data.data import Data 
+from Data.data import Data
+from Visualize.visualize import Visualize
 
-data_class = Data('./JSON')
+# Initialize the Data class
+data_class = Data(data_root='./JSON')
+
+# Initialize the Visualize class with the Data instance
+visualizer = Visualize(data_class)
+
+# print(data_class.frames[0])
 # print(data_class.preprocessed[0])
-print(data_class.preprocessed[1])
-print(data_class.moves[0])
-print(data_class.flattened_frames[34])
+# print(data_class.moves[0])
+# print(data_class.encoded_moves[0])
+print(data_class.flattened_frames[0])
 
-data_class.plot_lda('./vis_out/LDA/LDA_scaled_2.png', False)
-data_class.plot_lda_3d('./vis_out/LDA/LDA_scaled_2.png', False)
-# data_class.plot_lda(None, True)
+# Use the Visualize class for plotting
+# visualizer.plot_lda('./vis_out/LDA/LDA_2D.png', False)
+# visualizer.plot_lda_3d('./vis_out/LDA/LDA_3D.png', False)
+# visualizer.plot_mds('./vis_out/MDS/MDS_3D.png', False)
 
-# data_class.plot_mds(None, True)
-# data_class.plot_mds('./vis_out/MDS_3d.png', False)
+visualizer.plot_tsne(path='./vis_out/TSNE/TSNE_2D.png', show=False)
+
+# Uncomment to show plots
+# visualizer.plot_lda(None, True)

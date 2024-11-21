e13db03255506a2c4d28d1ecc3e820d4c085e284
lee-ranaweer <lee.ranaweer@gmail.com>
2024-11-21 13:10:08 -0500

    Accuracy up to 85%

--- Data/data.py
+++ Data/data.py
@@ -1,7 +1,7 @@
 import os
 import json
 import numpy as np
-from scipy.spatial.distance import pdist, squareform
+from scipy.spatial.distance import pdist, squareform, cdist
 from sklearn.manifold import MDS
 from sklearn.model_selection import train_test_split
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis 
@@ -38,122 +38,6 @@ class Data:
     lda = LinearDiscriminantAnalysis(n_components=n)
     lda_out = lda.fit_transform(X_scaled, y)
     return lda_out
-    
-  def plot_lda(self, path = None, show = False):
-    y = self.encoded_moves
-
-    # Split into train and test for more realistic scenario (optional)
-    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
-    X_r2 = self.lda_2
-    
-    # Plot the results
-    plt.figure(figsize=(15, 15))
-
-    target_names = ['up', 'down', 'left', 'right']
-    labels = [0, 1, 2, 3]
-    colors = ['blue', 'green', 'red', 'purple']
-    for color, label in zip(colors, labels):
-        idx = y == label
-        plt.scatter(X_r2[idx, 0], X_r2[idx, 1], color=color, label=target_names[label], alpha=0.7)
-
-    plt.title("LDA Representation of Battlesnake Moves")
-    plt.xlabel("LDA Component 1")
-    plt.ylabel("LDA Component 2")
-    plt.legend(loc="best")
-    
-    if (show):
-      plt.show()
-     
-    if(path !=  None): 
-      plt.savefig(path, format='png', bbox_inches='tight', dpi=400) # PNG
-    plt.clf()
-    
-    score = silhouette_score(X_r2, y)
-    print(f'Silhouette Score after LDA: {score}')
-    
-    db_index = davies_bouldin_score(X_r2, y)
-    print(f'Davies-Bouldin Index after LDA: {db_index}')
-      
-  def plot_lda_3d(self, path=None, show=False):
-    y = self.encoded_moves
-
-    # Initialize LDA with 3 components for 3D plotting
-    X_r3 = self.lda_3
-    
-    # Create a 3D plot
-    fig = plt.figure(figsize=(15, 15))
-    ax = fig.add_subplot(111, projection='3d')
-
-    target_names = ['up', 'down', 'left', 'right']
-    labels = [0, 1, 2, 3]
-    colors = ['blue', 'green', 'red', 'purple']
-    for color, label in zip(colors, labels):
-        idx = y == label
-        ax.scatter(X_r3[idx, 0], X_r3[idx, 1], X_r3[idx, 2], color=color, label=target_names[label], alpha=0.7)
-
-    # Set labels and title
-    ax.set_title("3D LDA Representation of Battlesnake Moves")
-    ax.set_xlabel("LDA Component 1")
-    ax.set_ylabel("LDA Component 2")
-    ax.set_zlabel("LDA Component 3")
-    ax.legend(loc="best")
-    
-    # Show or save the plot
-    if show:
-        plt.show()
-    if path is not None:
-        plt.savefig(path, format='png', bbox_inches='tight', dpi=400)
-    plt.clf()
-    
-    # Calculate and print clustering scores
-    score = silhouette_score(X_r3, y)
-    print(f'Silhouette Score after 3D LDA: {score}')
-    
-    db_index = davies_bouldin_score(X_r3, y)
-    print(f'Davies-Bouldin Index after 3D LDA: {db_index}')
-
-  def plot_mds(self, path = None, show = False):    
-    euclidean_data = self.__calculate_euclidean_distance()
-    mds = self.__multi_dim_scaling(euclidean_data)
-    
-    fig = plt.figure(figsize=(15,15))
-    # ax = fig.add_subplot(projection='3d')
-    ax = fig.add_subplot()
-    
-    plt.title('MDS Representation of Battlesnake Moves')
-
-    unique_classes = list(set(label['Move'] for label in self.moves))
-    colors = plt.cm.get_cmap('tab10', len(unique_classes))  # Using a colormap with enough distinct colors
-    class_colors = {cls: colors(i) for i, cls in enumerate(unique_classes)}
-
-    for i, label in enumerate(self.moves):
-        cls = label['Move']
-        
-        # Plot each point with its respective color
-        ax.scatter(mds[i, 0], mds[i, 1], color=class_colors[cls], s=10)
-        # ax.scatter(mds[i, 0], mds[i, 1], mds[i,2], color=class_colors[cls], s=10)
-        
-        # Annotate each point with its index
-        ax.annotate(str(i),
-                    (mds[i, 0], mds[i, 1]),            # Point to annotate
-                    textcoords="offset points",        # Position relative to the point
-                    xytext=(5, 5),                     # Offset of the annotation text
-                    ha='center', color='red') 
-        # ax.text(mds[i, 0], mds[i, 1], mds[i, 2], str(i),
-        #     color='red', ha='center')
-
-    for cls, color in class_colors.items():
-      ax.scatter([], [], color=color, label=cls)
-
-    # Add legend and show the plot
-    ax.legend(title="Classes")
-
-    if (show):
-      plt.show()
-     
-    if(path !=  None): 
-      plt.savefig(path, format='png', bbox_inches='tight', dpi=400) # PNG
-    plt.clf()
   
   def __read_frames(self):
     frames = []
@@ -174,6 +58,7 @@ class Data:
     for game_state in self.frames:
         turn = game_state["Turn"]
         food = game_state["Food"]
+        food = [(f["X"], f["Y"]) for f in food]
         snakes = game_state["Snakes"]
 
         # Initialize lists for player and enemy snakes
@@ -193,13 +78,30 @@ class Data:
         if player_body is None:
             continue  # Skip if the player's snake is not found
 
+        # Calculate the distance between the player's snake head and the enemy's snake head
+        player_head = player_body[0]
+        enemy_head = enemy_body[0]
+
+        opp_distance = self.calculate_snake_distance(player_head, enemy_body)
+        player_distance = self.calculate_snake_distance(enemy_head, player_body)
+        pairwise_distance = self.calculate_pairwise_distance(enemy_body, player_body)
+        wall_distance = self.calculate_wall_distance(player_body)
+        food_distance = self.calculate_food_distance(player_body, food)
+        valid_spaces = self.calculate_valid_moves(player_body, enemy_body)
+
         # Flatten features into a vector (simple representation)
         features = {
             "turn": turn,
-            "food_positions": [(f["X"], f["Y"]) for f in food],
+            "food_positions": food,
             "player_body": player_body,
             "enemy_body": enemy_body,
             "health": player_health,
+            "opp_distance": opp_distance,
+            "player_distance": player_distance,
+            "pairwise_distance": pairwise_distance,
+            "food_distance": food_distance,
+            "wall_distance": wall_distance,
+            "valid_spaces": valid_spaces
         }
         preprocessed.append(features)
 
@@ -256,20 +158,27 @@ class Data:
   def __flatten_frames(self):
     flattened_frames = []
     for i, frame in enumerate(self.preprocessed):
-      flattened_frames.append(self.__flatten_frame_len_to_board(frame, i+1))
+      flattened_frames.append(self.__flatten_frame_i_to_list(frame, i+1))
       
     return np.array(flattened_frames)
   
   def __flatten_frame_i_to_list(self, frame, i):
 
     # add length to arrays
-    return np.append(self.__flatten_frame_to_list(frame, i), [len(frame["player_body"]), len(frame["enemy_body"]), len(frame["food_positions"]), i, frame["health"]], axis=0)
+    return np.append(self.__flatten_frame_to_list(frame), [len(frame["player_body"]), len(frame["enemy_body"]), len(frame["food_positions"]), frame["health"], i], axis=0)
     # return np.concatenate([food_positions_padded, player_body_padded, enemy_bodies_padded])
   
-  def __flatten_frame_to_list(self, frame, i):
+  def __flatten_frame_to_list(self, frame):
     food_positions = [coord for f in frame["food_positions"] for coord in f]
     player_body = [coord for s in frame["player_body"] for coord in s]
     enemy_bodies = [coord for b in frame["enemy_body"] for coord in b]
+    wall_distance = [dist for b in frame["wall_distance"] for dist in b]
+    food_distance = [dist for b in frame["food_distance"] for dist in b]
+    opp_distance = frame["opp_distance"]
+    player_distance = frame["player_distance"]
+    pairwise_distance = frame["pairwise_distance"]
+    valid_spaces = frame["valid_spaces"]
+
      # Define the desired length for padding
     desired_length = 100
 
@@ -283,8 +192,14 @@ class Data:
     player_body_padded = pad_to_length(player_body, desired_length)
     enemy_bodies_padded = pad_to_length(enemy_bodies, desired_length)
 
+    opp_distance_padded = pad_to_length(opp_distance, desired_length)
+    player_distance_padded = pad_to_length(player_distance, desired_length)
+    pairwise_distance_padded = pad_to_length(pairwise_distance, 2000)
+    wall_distance_padded = pad_to_length(wall_distance, 400)
+    food_distance_padded = pad_to_length(food_distance, 400)
+
     # Concatenate all arrays
-    return np.concatenate([food_positions_padded, player_body_padded, enemy_bodies_padded])
+    return np.concatenate([food_positions_padded, player_body_padded, enemy_bodies_padded, player_distance_padded, opp_distance_padded, pairwise_distance_padded, wall_distance_padded, food_distance_padded, valid_spaces])
   
   def __flatten_frame_to_board(self, frame, i):
     # Convert the board into a flattened array
@@ -355,3 +270,55 @@ class Data:
   def multi_dim_scaling(self, data, dim = 2):
     mds = MDS(n_components=dim, random_state=0, dissimilarity='precomputed')  
     return mds.fit_transform(data)
+  
+  def calculate_snake_distance(self, head, body):
+    return np.array([np.sum(np.abs(np.array(head) - np.array(segment))) for segment in body])
+
+  def calculate_pairwise_distance(self, body1, body2):
+    return cdist(body1, body2, 'cityblock').ravel()
+  
+  def calculate_wall_distance(self, body, board_size=11):
+    # Distance from upper
+      # 11 - y
+    # Distance from lower
+      # y
+    # Distance from left
+      # x
+    # Distance from right
+      # 11 - x
+
+    distances = []
+
+    for segment in body:
+        x, y = segment
+        distance_to_top = board_size - y
+        distance_to_right = board_size - x
+        distances.append([distance_to_top, distance_to_right])
+
+    return np.array(distances)
+  
+  def calculate_food_distance(self, body, foods):
+    return np.array([[np.sum(np.abs(np.array(segment) - np.array(food))) for food in foods] for segment in body])
+  
+  def calculate_valid_moves(self, player, opp, board_size=11):
+      mask = [1, 1, 1, 1]  # Initialize mask for [up, down, left, right]
+
+      head_x, head_y = player[0]  # Get the head position of the player's snake
+
+      # Check up move
+      if head_y + 1 >= board_size or (head_x, head_y + 1) in player or (head_x, head_y + 1) in opp:
+          mask[0] = 0
+
+      # Check down move
+      if head_y - 1 < 0 or (head_x, head_y - 1) in player or (head_x, head_y - 1) in opp:
+          mask[1] = 0
+
+      # Check left move
+      if head_x - 1 < 0 or (head_x - 1, head_y) in player or (head_x - 1, head_y) in opp:
+          mask[2] = 0
+
+      # Check right move
+      if head_x + 1 >= board_size or (head_x + 1, head_y) in player or (head_x + 1, head_y) in opp:
+          mask[3] = 0
+
+      return mask
--- visualization.py
+++ visualization.py
@@ -8,25 +8,33 @@ data_class = Data(data_root='./JSON')
 visualizer = Visualize(data_class)
 
 # print(data_class.frames[0])
-# print(data_class.preprocessed[0])
+print(data_class.preprocessed[9])
 # print(data_class.moves[0])
 # print(data_class.encoded_moves[0])
-print(data_class.flattened_frames[0])
+# print(data_class.flattened_frames[0])
+
+# print(data_class.preprocessed[50]['opp_distance']) # Distance of player head to opponent body
+
+# print()
+# print()
+# print()
+
+# print(data_class.preprocessed[50]['player_distance']) # Distance of opponent head to player body
 
 # Use the Visualize class for plotting
 # visualizer.plot_lda('./vis_out/LDA/LDA_2D.png', False)
 # visualizer.plot_lda_3d('./vis_out/LDA/LDA_3D.png', False)
 # visualizer.plot_mds('./vis_out/MDS/MDS_3D.png', False)
 
-visualizer.plot_tsne(path='./vis_out/TSNE/TSNE_2D.png', show=False)
+# visualizer.plot_tsne(path='./vis_out/TSNE/TSNE_2D.png', show=False)
 
 # Uncomment to show plots
 # visualizer.plot_lda(None, True)
 # data_class.plot_lda('./vis_out/LDA/LDA_scaled_5_health.png', False)
-# data_class.plot_lda(None, True)
+visualizer.plot_lda(None, False)
 
 # data_class.plot_lda_3d('./vis_out/LDA/LDA_scaled_3d_3_health.png', False)
-data_class.plot_lda_3d(None, True)
+visualizer.plot_lda_3d(None, False)
 
 # data_class.plot_mds(None, True)
 # data_class.plot_mds('./vis_out/MDS_3d.png', False)
